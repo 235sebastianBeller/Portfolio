3e52849bfef7042b4f1cfdef633eb248de4d4f0c
978sebasBeller <sebastian.beller@ucb.edu.bo>
2024-12-20 00:18:35 -0400

    Se mejora lo responsivo de la pagina projects

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -8,7 +8,7 @@ const PortfolioPage = () => {
 
     return (
       <div className="flex items-center justify-center">
-            <div className="flex flex-col justify-center  mt-28 xl:mt-6  h-1/4">
+            <div className="flex flex-col justify-center  mt-28 md:mt-6  h-1/4">
                 <h1 className="text-2xl leading-tight text-center md:text-3xl md:mb-5">Mis proyectos</h1>
 
                 <div className="text-center relative z-10 grid max-w-5xl gap-5 mx-auto mt-1 md:grid-cols-3">
